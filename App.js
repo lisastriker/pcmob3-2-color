d09159560ffe941763c91a9072ffef5033d85f13
lisastriker <lisastriker@gmail.com>
2020-12-08 11:27:58 +0800

    Why not working

--- App.js
+++ App.js
@@ -3,25 +3,47 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createStackNavigator} from "@react-navigation/stack";
 import {NavigationContainer} from  "@react-navigation/native";
-import BlockRBG from "./components/BlockRGB"
+import BlockRGB from "./components/BlockRGB"
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 
  const COLORS = [
    { red: 255, green: 128, blue: 0, id: "0" },
    { red: 0, green: 128, blue: 255, id: "1" },
    { red: 128, green: 0, blue: 255, id: "2" },
+   { red:0, green:0, blue:0, id:"3"}
  ];
 
 function HomeScreen(){
    const [colorArray, setColorArray] = useState(COLORS);
    function renderItems({item}){
-    return <BlockRBG red={item.red} green={item.green} blue={item.blue}/>;
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue}/>;
+  }
+
+  function addColor() {
+    let newColor = {
+      red: Math.floor(Math.random() * 256),
+      green: Math.floor(Math.random() * 256),
+      blue: Math.floor(Math.random() * 256),
+      id: colorArray.length.toString(),
+    };
+    setColorArray([...colorArray, newColor]);
+  }
+
+  function resetColor(){
+    setColorArray([]);
   }
 
   return(
-    <View>
-      <TouchableOpacity onPress={addColor}>
-        <Text>Add Color</Text>
+    <View style={styles.container}>
+       <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={addColor}>
+        <Text style={{ color: "red" }}>Add Colour</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={resetColor}>
+        <Text style={{ color: "red" }}>Reset Colour</Text>
       </TouchableOpacity>
       <FlatList style={{width:"100%"}} data={colorArray} renderItem={renderItems}/>
       </View>
