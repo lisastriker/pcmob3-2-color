b59796af8e32cb1164a68c3c5ceeef36d4541eb1
lisastriker <lisastriker@gmail.com>
2020-12-08 10:09:53 +0800

    I'm sleepy but i did in the color blocks

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {createStackNavigator} from "@react-navigation/stack"
-import {NavigationContainer,} from  "@react-naigation/native"
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from  "@react-navigation/native";
+import BlockRBG from "./components/BlockRGB"
+
+
+function HomeScreen(){
+  return(
+    <View>
+      <BlockRBG red={100} blue={50} green={255}/>
+      <BlockRBG red={255} blue={0} green={255}/>
+      <BlockRBG red={0} blue={200} green={255}/>
+    </View>
+  )
+}
+const Stack = createStackNavigator()
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={homeScreen}>
+        <Stack.Screen name="Home" component={HomeScreen}>
 
         </Stack.Screen>
       </Stack.Navigator>
@@ -15,7 +29,7 @@ export default function App() {
   );
 }
 
-const stack = createStackNavigator
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/BlockRGB.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function BlockRBG(props){
+    return(
+        <View
+            style={{
+                backgroundColor: `rgb(${props.red}, ${props.green}, ${props.blue})`,
+                padding: 30,
+                width:"100%",
+            }}>
+        </View>
+    )
+}
